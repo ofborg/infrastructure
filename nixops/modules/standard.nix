3f83b5c14a3f5b21f3c9edcbdd89e8022698c100
Graham Christensen <graham@grahamc.com>
2018-02-25 13:22:06 -0500

    Update defeault networking configs

--- nixops/modules/standard.nix
+++ nixops/modules/standard.nix
@@ -8,9 +8,16 @@ in {
     nixpkgs.config.allowUnfree = true;
 
     services.openssh.enable = true;
-    networking.firewall = {
-      enable = true;
-      allowedTCPPorts = [ 22 ];
+    networking = {
+      nameservers = [
+        "4.2.2.1"
+        "4.2.2.2"
+        "2001:4860:4860::8888"
+      ];
+      firewall = {
+        enable = true;
+        allowedTCPPorts = [ 22 ];
+      };
     };
 
     users = {
