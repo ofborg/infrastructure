ac95222236e84a91074ca2ab5ca9d8646496db24
Graham Christensen <graham@grahamc.com>
2018-03-02 17:33:57 -0500

    management / service account

--- terraform/rabbitmq.tf
+++ terraform/rabbitmq.tf
@@ -28,6 +28,14 @@ variable "rabbitmq_monitoring_password" {
   type = "string"
 }
 
+variable "rabbitmq_ofborgservice_username" {
+  type = "string"
+}
+
+variable "rabbitmq_ofborgservice_password" {
+  type = "string"
+}
+
 
 # Configure the RabbitMQ provider
 provider "rabbitmq" {
@@ -42,10 +50,38 @@ resource "rabbitmq_user" "terraform" {
   tags     = ["administrator", "management"]
 }
 
+resource "rabbitmq_permissions" "terraform-access" {
+  user  = "${rabbitmq_user.terraform.name}"
+  vhost = "${rabbitmq_vhost.ofborg.name}"
+
+  permissions {
+    configure = ".*"
+    write     = ".*"
+    read      = ".*"
+  }
+}
+
 resource "rabbitmq_user" "monitoring" {
   name     = "${var.rabbitmq_monitoring_username}"
   password = "${var.rabbitmq_monitoring_password}"
   tags     = [ "management"]
+}
+
+resource "rabbitmq_user" "ofborgservice" {
+  name     = "${var.rabbitmq_ofborgservice_username}"
+  password = "${var.rabbitmq_ofborgservice_password}"
+  tags     = [ "management"]
+}
+
+resource "rabbitmq_permissions" "ofborgservice-access" {
+  user  = "${rabbitmq_user.ofborgservice.name}"
+  vhost = "${rabbitmq_vhost.ofborg.name}"
+
+  permissions {
+    configure = ".*"
+    write     = ".*"
+    read      = ".*"
+  }
 }
 
 # Create a virtual host
