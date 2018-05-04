9dbb951df0112cfe2d3d3d293d52ffb5e310d6ad
Graham Christensen <graham@grahamc.com>
2018-05-04 07:17:28 -0400

    Run GC much more frequently

--- nixops/modules/standard.nix
+++ nixops/modules/standard.nix
@@ -101,7 +101,7 @@ in {
 
       gc = {
         automatic = true;
-        dates = "8:44";
+        dates = "*:0/15";
 
         options = ''
           --max-freed "$((${toString config.nix.gc_free_gb} * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"
