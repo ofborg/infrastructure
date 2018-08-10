e1154d72e7772981f8de88e40dd1d9f80065b400
Graham Christensen <graham@grahamc.com>
2018-08-09 23:41:31 -0400

    Replace core-1

--- terraform/nixops-generated/core-1.nix
+++ terraform/nixops-generated/core-1.nix
@@ -1,12 +1,12 @@
 {
-  deployment.targetHost = "147.75.39.187";
+  deployment.targetHost = "147.75.39.219";
   terraform.name = "core-1";
       packet.plan = "baremetal_0";
     networking.hostName = "core-1.ewr1.nix.ci";
 
     roles.core-v2.enable = true;
     packet.network_data = ''
-      [{"address":"147.75.39.187","cidr":"31","family":"4","gateway":"147.75.39.186","public":"1"},{"address":"2604:1380:0:3c00::7","cidr":"127","family":"6","gateway":"2604:1380:0:3c00::6","public":"1"},{"address":"10.99.147.135","cidr":"31","family":"4","gateway":"10.99.147.134","public":"0"}]
+      [{"address":"147.75.39.219","cidr":"31","family":"4","gateway":"147.75.39.218","public":"1"},{"address":"2604:1380:0:3c00::1","cidr":"127","family":"6","gateway":"2604:1380:0:3c00::","public":"1"},{"address":"10.99.147.129","cidr":"31","family":"4","gateway":"10.99.147.128","public":"0"}]
     '';
   
 }
