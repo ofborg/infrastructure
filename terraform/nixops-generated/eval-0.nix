d7c55879016d323b1e40cae5cc8b8f718ee91b95
Graham Christensen <graham@grahamc.com>
2020-04-01 15:51:10 -0400

    Drop expensive evaluators

--- terraform/nixops-generated/eval-0.nix
+++
@@ -1,9 +0,0 @@
-{
-  deployment.targetHost = "195.201.32.12";
-  terraform.name = "eval-0";
-      hetzner.plan = "cx41";
-    networking.hostName = "eval-0-gleber.ewr1.nix.ci";
-
-    roles.evaluator.enable = true;
-  
-}
--- terraform/nixops-generated/eval-2.nix
+++
@@ -1,9 +0,0 @@
-{
-  deployment.targetHost = "195.201.33.170";
-  terraform.name = "eval-2";
-      hetzner.plan = "cx41";
-    networking.hostName = "eval-2-shlevy.ewr1.nix.ci";
-
-    roles.evaluator.enable = true;
-  
-}
