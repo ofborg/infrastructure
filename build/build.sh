144323416794960d1c1b9cd2c3cb6015658a5fa6
Cole Helbling <cole.e.helbling@outlook.com>
2020-05-28 15:57:29 -0700

    confirm kill-obsolete
    Otherwise:
    
        Warning: are you sure you want to destroy Packet.Net machine ‘packet-spot-eval-6’? (y/N)

--- build/build.sh
+++ build/build.sh
@@ -39,5 +39,5 @@ else
     # core-0. To achieve this, we first deploy only to core-0, followed by a
     # deploy to the rest of the hosts, where obsolete hosts will be dropped.
     nixops deploy --check --allow-recreate --include core-0
-    nixops deploy --check --allow-recreate --kill-obsolete --exclude core-0
+    nixops deploy --check --allow-recreate --kill-obsolete --exclude core-0 --confirm
 fi
