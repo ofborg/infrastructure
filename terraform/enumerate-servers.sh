cc94d324e3c0eaa3cf230f968e378272710b30c5
Graham Christensen <graham@grahamc.com>
2021-01-13 18:25:36 -0500

    terraform init -> stderr; gosh durn it.

--- terraform/enumerate-servers.sh
+++ terraform/enumerate-servers.sh
@@ -5,7 +5,7 @@ set -eux
 set -o pipefail
 
 cd "$(dirname "$0")/base"
-terraform init
+terraform init >&2
 terraform output -json \
   | jq .deploy_targets.value \
   | jq -cr '. as $input | keys | map(. as $name | { key: $name, value: $input[$name]}) | .[]'
