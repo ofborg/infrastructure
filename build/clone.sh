5a8e2b26f27a335cdf4aa05cbb4df873d6b81760
Graham Christensen <graham@grahamc.com>
2021-01-13 19:30:41 -0500

    pipeline: clone repos on all deploy steps

---
+++ build/clone.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash ../shell.nix
+#shellcheck shell=bash
+
+echo "$NIX_PATH"
+
+set -eux
+
+fetchrepo() {
+    dest=$1
+    src=$2
+    branch=$3
+    remote=$(echo "$src" | md5sum | cut -d' ' -f1)
+
+    if [ ! -d "$dest" ]; then
+        git clone "$src" "$dest"
+    fi
+
+    (
+        cd "$dest"
+        if ! git remote | grep -q "$remote"; then
+            git remote add "$remote" "$src"
+        fi
+
+        git fetch "$remote"
+        git clean -dfx
+        git checkout "$remote/$branch"
+    )
+}
+
+mkdir -p repos
+fetchrepo repos/ofborg https://github.com/nixos/ofborg.git released
+fetchrepo repos/log-viewer https://github.com/samueldr/ofborg-viewer.git master
+
