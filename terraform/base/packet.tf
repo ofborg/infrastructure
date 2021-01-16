bd65edf55ca65be87b2f268ba6920e121950b38c
Graham Christensen <graham@grahamc.com>
2021-01-15 22:06:17 -0500

    base TF: prune commented code

--- terraform/base/packet.tf
+++ terraform/base/packet.tf
@@ -145,7 +145,10 @@ USERDATA
     }
   })
 
-  # ipxe_script_url     = "http://images.platformequinix.net/nixos/installer-pre2/x86/netboot.ipxe"
+  # In the future, try:
+  # http://images.platformequinix.net/nixos/installer-pre2/x86/netboot.ipxe
+  # This URL was causing issues before, but should be identical to the netboot.gsc.io
+  # URL on an ongoing basis.
   ipxe_script_url     = "https://netboot.gsc.io/installer-pre2/x86/netboot.ipxe"
   always_pxe       = false
   tags                = concat(var.tags, ["core-0", "skip-hydra"])
@@ -169,91 +172,6 @@ resource "metal_device" "evaluator" {
 ${var.bootstrap_expr}
 USERDATA
 
-#  custom_data = jsonencode({
-#    cpr_storage = {
-#      disks = [
-#        {
-#          device = "/dev/disk/by-packet-category/boot1"
-#          partitions = [
-#            {
-#              label = "BIOS"
-#              number = 1
-#              size = "8M"
-#            },
-#            {
-#              label = "BOOT"
-#              number = 2
-#              size = "512M"
-#            },
-#            {
-#              label = "ROOT"
-#              number = 3
-#              size = 0
-#            },
-#          ]
-#        },
-#      ]
-#
-#      filesystems = [
-#        {
-#          mount = {
-#            device = "/dev/disk/by-packet-category/boot1-part2"
-#            format = "vfat"
-#            point = "/boot"
-#            create = { options = [
-#              "32",
-#              "-n",
-#              "EFI"
-#            ] }
-#          }
-#        },
-#      ]
-#    }
-#
-#    cpr_zfs = {
-#      pools = { npool = { vdevs = [
-#        {
-#          disk = [
-#            "/dev/disk/by-packet-category/boot1-part3",
-#            "/dev/disk/by-packet-category/storage1",
-#            "/dev/disk/by-packet-category/storage2",
-#            "/dev/disk/by-packet-category/storage3",
-#            "/dev/disk/by-packet-category/storage4",
-#            "/dev/disk/by-packet-category/storage5",
-#            "/dev/disk/by-packet-category/storage6",
-#          ]
-#        },
-#      ] } }
-#
-#      datasets = {
-#        "npool/root" = { properties = { mountpoint = "legacy" } }
-#        "npool/nix" = { properties = { mountpoint = "legacy" } }
-#        "npool/home" = { properties = { mountpoint = "legacy" } }
-#        "npool/var" = { properties = { mountpoint = "legacy" } }
-#      }
-#
-#      mounts = [
-#        {
-#          dataset = "npool/root"
-#          point = "/"
-#        },
-#        {
-#          dataset = "npool/nix"
-#          point = "/nix"
-#        },
-#        {
-#          dataset ="npool/home"
-#          point ="/home"
-#        },
-#        {
-#          dataset ="npool/var"
-#          point ="/var"
-#        }
-#      ]
-#    }
-#  })
-
-  # ipxe_script_url     = "http://images.platformequinix.net/nixos/installer-pre2/x86/netboot.ipxe"
   ipxe_script_url     = "https://netboot.gsc.io/installer-pre2/x86/netboot.ipxe"
   always_pxe       = false
   tags                = concat(var.tags, ["evaluator", "skip-hydra"])
