0a2e3ab8fbafc14a181df47f7293be2e4fdf0201
Graham Christensen <graham@grahamc.com>
2021-01-15 22:00:00 -0500

    nixops/modules/default.nix: fixup indentation

--- nixops/modules/default.nix
+++ nixops/modules/default.nix
@@ -1,19 +1,19 @@
 {
   imports = [
- ./../../private/local.nix
-   ./site.nix
-   ./standard.nix
-   ./secrets.nix
-   ./roles.nix
-   ./terraform.nix
-   ./hetzner.nix
-   ./ofborg
-   ./rabbitmq
-   ./webhook
-   ./phpfpm.nix
-   ./monitoring.nix
-   ./logging.nix
-   ./log-viewer.nix
-   ./website.nix
-];
+    ./../../private/local.nix
+    ./site.nix
+    ./standard.nix
+    ./secrets.nix
+    ./roles.nix
+    ./terraform.nix
+    ./hetzner.nix
+    ./ofborg
+    ./rabbitmq
+    ./webhook
+    ./phpfpm.nix
+    ./monitoring.nix
+    ./logging.nix
+    ./log-viewer.nix
+    ./website.nix
+  ];
 }
