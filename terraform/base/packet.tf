57e2bf9ca9fe8755974e4124598f4fb804aabc89
Graham Christensen <graham@grahamc.com>
2021-04-26 13:41:37 -0400

    add the metal type to core and evaluators

--- terraform/base/packet.tf
+++ terraform/base/packet.tf
@@ -6,11 +6,13 @@ output "deploy_targets" {
         core = {
           ip = metal_device.ofborg-core.network.0.address
           expression = "{ roles.core.enable = true; }"
+          provisioner = "metal"
         },
       },
       {for e in metal_device.evaluator : e.hostname => {
         ip = e.network.0.address
         expression = "{ services.ofborg = { builder.enable = true; evaluator.enable = true; }; }"
+        provisioner = "metal"
       }},
   )
 }
