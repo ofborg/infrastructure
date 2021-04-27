d30a7ce06247bc22aa0919d54a8f3d37f7f79f4c
Graham Christensen <graham@floxdev.com>
2021-04-27 11:08:48 -0400

    logging.nix: format

--- nixops/modules/logging.nix
+++ nixops/modules/logging.nix
@@ -1,19 +1,17 @@
 { nodes, pkgs, config, lib, ... }:
-let
-  cfg = config.services.ofborg.monitoring;
+let cfg = config.services.ofborg.monitoring;
 in {
   config = lib.mkIf cfg.enable {
-    networking.firewall.extraCommands = builtins.concatStringsSep "\n" (lib.mapAttrsToList
-      (name: node:
-        ''
-          # ${name}
-          ${if node.config.deployment.targetHost != null then ''
+    networking.firewall.extraCommands = builtins.concatStringsSep "\n"
+      (lib.mapAttrsToList (name: node: ''
+        # ${name}
+        ${if node.config.deployment.targetHost != null then ''
           iptables -A nixos-fw -p tcp \
             --dport 3100 --source ${node.config.deployment.targetHost}/32 \
             --jump nixos-fw-accept
             '' else ''
-              #     (null)
-            ''}'') nodes);
+            #     (null)
+          ''}'') nodes);
 
     services.loki = {
       enable = true;
