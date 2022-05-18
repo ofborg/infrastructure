51ef66f7721cc95283a0a75616676ae6b7428c7c
Cole Helbling <cole.e.helbling@outlook.com>
2022-05-18 09:58:26 -0700

    core-next: add to terraform temporarily

--- terraform/base/packet.tf
+++ terraform/base/packet.tf
@@ -9,6 +9,13 @@ output "deploy_targets" {
         provisioner = "metal"
       },
     },
+    {
+      core-next = {
+        ip          = metal_device.ofborg-core-next.network.0.address
+        expression  = "{ roles.core.enable = true; }"
+        provisioner = "metal"
+      },
+    },
     { for e in metal_device.evaluator : e.hostname => {
       ip          = e.network.0.address
       expression  = "{ services.ofborg = { builder.enable = true; evaluator.enable = true; }; }"
@@ -154,6 +161,127 @@ USERDATA
   ipxe_script_url = "https://netboot.gsc.io/installer-pre2/x86/netboot.ipxe"
   always_pxe      = false
   tags            = concat(var.tags, ["core-0", "skip-hydra"])
+
+  lifecycle {
+    ignore_changes = [user_data]
+  }
+}
+
+resource "metal_device" "ofborg-core-next" {
+  project_id       = var.project_id
+  hostname         = "ofborg-core-next"
+  billing_cycle    = "hourly"
+  operating_system = "custom_ipxe"
+  plan             = "c3.small.x86"
+  facilities       = ["dc13"]
+
+  user_data = <<USERDATA
+#!nix
+${var.bootstrap_expr}
+USERDATA
+
+  custom_data = jsonencode({
+    cpr_storage = {
+      disks = [
+        {
+          device = "/dev/sda"
+          partitions = [
+            {
+              label  = "BIOS"
+              number = 1
+              size   = "8M"
+            },
+            {
+              label  = "BOOT"
+              number = 2
+              size   = "512M"
+            },
+            {
+              label  = "ROOT"
+              number = 3
+              size   = 0
+            },
+          ]
+        },
+        {
+          device = "/dev/sdb"
+          partitions = [
+            {
+              label  = "SECONDBIOS"
+              number = 1
+              size   = "8M"
+            },
+            {
+              label  = "SECONDBOOT"
+              number = 2
+              size   = "512M"
+            },
+            {
+              label  = "SECONDROOT"
+              number = 3
+              size   = 0
+            },
+          ]
+        },
+      ]
+
+      filesystems = [
+        {
+          mount = {
+            device = "/dev/sda2"
+            format = "vfat"
+            point  = "/boot"
+            create = { options = [
+              "32",
+              "-n",
+              "EFI"
+            ] }
+          }
+        },
+      ]
+    }
+
+    cpr_zfs = {
+      pools = { npool = { vdevs = [
+        {
+          disk = [
+            "/dev/sda3",
+            "/dev/sdb3"
+          ]
+        },
+      ] } }
+
+      datasets = {
+        "npool/root" = { properties = { mountpoint = "legacy" } }
+        "npool/nix"  = { properties = { mountpoint = "legacy" } }
+        "npool/home" = { properties = { mountpoint = "legacy" } }
+        "npool/var"  = { properties = { mountpoint = "legacy" } }
+      }
+
+      mounts = [
+        {
+          dataset = "npool/root"
+          point   = "/"
+        },
+        {
+          dataset = "npool/nix"
+          point   = "/nix"
+        },
+        {
+          dataset = "npool/home"
+          point   = "/home"
+        },
+        {
+          dataset = "npool/var"
+          point   = "/var"
+        }
+      ]
+    }
+  })
+
+  ipxe_script_url = "http://01ad16e6.packethost.net:3030/dispatch/hydra/01ad16e6.packethost.net/nixos-install-equinix-metal/release/c3-small-x86"
+  always_pxe      = false
+  tags            = concat(var.tags, ["core-0-next", "skip-hydra"])
 
   lifecycle {
     ignore_changes = [user_data]
