189a2b90761fb4bf5122f3ceb5573a112b2dfcf9
Cole Helbling <cole.e.helbling@outlook.com>
2022-10-26 09:49:10 -0700

    Expose the nix-darwin configuration as a flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "ofborg infrastructure";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    darwin = { url = "github:LnL7/nix-darwin"; inputs.nixpkgs.follows = "nixpkgs"; };
+  };
+
+  outputs =
+    { nixpkgs
+    , darwin
+    , ...
+    }@inputs:
+    {
+      darwinConfigurations =
+        let
+          mac = system: darwin.lib.darwinSystem {
+            inherit system;
+
+            modules = [
+              ./darwin-configuration.nix
+            ];
+          };
+        in
+        {
+          arm64 = mac "aarch64-darwin";
+          x86_64 = mac "x86_64-darwin";
+        };
+    };
+}
